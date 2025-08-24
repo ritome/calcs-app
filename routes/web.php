9e2b683457ab0846fef8677f6b2e6b115d43fa3e
ritome <ritome0109@gmail.com>
2025-08-25 08:22:26 +0900

    計算アプリ表示

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Volt::route('/calcs', 'hello');
+Volt::route('/calcs/{a}/{op}/{b}', 'calc')
+    ->whereNumber('a')  // 整数のみ
+    ->whereNumber('b'); // 整数のみ
